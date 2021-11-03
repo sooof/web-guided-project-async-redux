fb6722425a1d0d5add14fd11c18327aa26203466
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-03 12:52:54 -0400

    lecture work-in-progress

--- thunk/src/reducers/index.js
+++ thunk/src/reducers/index.js
@@ -1,17 +1,7 @@
 const initialState = {
   person: {
-    name: {
-      title: "Mr",
-      first: "Silas",
-      last: "Petersen"
-    },
-    picture: {
-      large: "https://randomuser.me/api/portraits/men/70.jpg",
-      medium: "https://randomuser.me/api/portraits/med/men/70.jpg",
-      thumbnail: "https://randomuser.me/api/portraits/thumb/men/70.jpg"
-    }
   },
-  isFetching: false,
+  isFetching: true,
   error: ''
 };
 
