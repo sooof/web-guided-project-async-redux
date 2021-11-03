c631ed59cb848d071ee09b5cd95bdae76ecfd7c4
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-03 13:51:28 -0400

    lecture work-in-progress

--- thunk/src/index.js
+++ thunk/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+
+import logger from 'redux-logger';
 
 import App from './App';
 
