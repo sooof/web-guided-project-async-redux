404767faabf285c1dc41abe8d27f429e2c9bf077
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-03 13:09:35 -0400

    lecture work-in-progress

--- thunk/src/actions/index.js
+++ thunk/src/actions/index.js
@@ -3,3 +3,15 @@ export const FETCH_START = "FETCH_START";
 export const fetchStart = () => {
     return({type: FETCH_START});
 }
+
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+
+export const fetchSuccess = (person)=> {
+    return({type: FETCH_SUCCESS, payload});
+}
+
+export const FETCH_ERROR = "FETCH_ERROR";
+
+export const fetchSuccess = (person)=> {
+    return({type: FETCH_SUCCESS, payload});
+}
