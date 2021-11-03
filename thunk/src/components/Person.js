6f252bec84e36543dce16ed750bd9e97bab1382f
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-03 13:35:30 -0400

    lecture work-in-progress

--- thunk/src/components/Person.js
+++ thunk/src/components/Person.js
@@ -19,8 +19,7 @@ const Person = ({ person, isFetching, error, dispatch }) => {
     dispatch(fetchStart());
     axios.get('https://randomuser.me/api/')
       .then(resp=> {
-        // dispatch(fetchSuccess(resp.data.results[0]));
-        dispatch(fetchError(err));
+        dispatch(fetchSuccess(resp.data.results[0]));
       })
       .catch(err => {
         dispatch(fetchError(err));
