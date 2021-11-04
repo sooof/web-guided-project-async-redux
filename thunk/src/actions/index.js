40998174ff078bd6a5902faa593bc036474a148d
sooof <sooof@me.com>
2021-11-03 18:56:52 -0700

    Three { FETCH_START, FETCH_SUCCESS, FETCH_ERROR }

---
+++ thunk/src/actions/index.js
@@ -0,0 +1,11 @@
+
+export const FETCH_START = "FETCH_START";
+
+export const fetchStart = () => {
+    return({type: FETCH_START});
+}
+
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+
+
+export const FETCH_POKEMON_FAILURE = 'FETCH_POKEMON_FAILURE';
--- thunk/src/reducers/index.js
+++ thunk/src/reducers/index.js
@@ -1,3 +1,43 @@
+
+   
+import { FETCH_START, FETCH_SUCCESS, FETCH_ERROR } from './../actions';
+// display state
+// const initialState = {
+//   person: {
+//     name: {
+//       title: "Mr",
+//       first: "Silas",
+//       last: "Petersen"
+//     },
+//     picture: {
+//       large: "https://randomuser.me/api/portraits/men/70.jpg",
+//       medium: "https://randomuser.me/api/portraits/med/men/70.jpg",
+//       thumbnail: "https://randomuser.me/api/portraits/thumb/men/70.jpg"
+//     }
+//   },
+//   isFetching: false,
+//   //error: 'Hey we have an error'
+//   error: ''
+// };
+
+// Load state
+// const initialState = {
+//   person: {
+//   },
+//   isFetching: true,
+//   //error: 'Hey we have an error'
+//   error: ''
+// };
+// Error state
+// 1
+// const initialState = {
+//   person: {
+//   },
+//   isFetching: true,
+//   error: 'Hey we have an error' 
+// };
+// Error state
+// 2
 const initialState = {
   person: {
     name: {
@@ -12,11 +52,18 @@ const initialState = {
     }
   },
   isFetching: false,
-  error: ''
+  error: 'Hey we have an error'
 };
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_START:
+      return ({
+        ...state,
+        person: {},
+        isFinite: true,
+        error: ''
+      });
     default:
       return state;
   }
